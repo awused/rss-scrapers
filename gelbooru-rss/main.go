e97dd01760b2b4c4aaef9c1681e6abb78c52f4b5
Desuwa <desuwa@desuwa.net>
2022-07-17 13:49:25 -0700

    Add api key file and tag blacklist

--- gelbooru-rss/main.go
+++ gelbooru-rss/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"html"
 	"io/ioutil"
@@ -11,20 +13,23 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gorilla/feeds"
 	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/errors"
+	ldberrors "github.com/syndtr/goleveldb/leveldb/errors"
 )
 
 const indexURLPrefix = "https://gelbooru.com/index.php?page=post&s=list&tags="
-const indexAPIPrefix = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags="
-const tagsAPIPrefix = "https://gelbooru.com/index.php?page=dapi&s=tag&q=index&json=1&names="
+const indexAPIFormat = "https://gelbooru.com/index.php?page=dapi%s&s=post&q=index&json=1&tags=%s"
+const tagsAPIFormat = "https://gelbooru.com/index.php?page=dapi%s&s=tag&q=index&json=1&names=%s"
 const hrefPrefix = "https://gelbooru.com/index.php?page=post&s=view&id="
 const gelbooruRoot = "https://gelbooru.com/"
+
+var apiKey = ""
 
 type indexResponse struct {
 	Post []struct {
@@ -53,15 +58,19 @@ func main() {
 		log.Panic("Specify at least one tag")
 	}
 
+	apiKey = loadApiKey()
+
 	db := openTagsDB()
 	defer db.Close()
+
+	blacklist := loadBlacklist()
 
 	searchTags := []string{}
 	for _, v := range os.Args[1:] {
 		searchTags = append(searchTags, url.QueryEscape(v))
 	}
 
-	indexURL := indexAPIPrefix + strings.Join(searchTags, "+")
+	indexURL := fmt.Sprintf(indexAPIFormat, apiKey, strings.Join(searchTags, "+"))
 
 	resp, err := http.Get(indexURL)
 	if err != nil {
@@ -78,8 +87,10 @@ func main() {
 	var b indexResponse
 	err = json.Unmarshal(body, &b)
 	if err != nil {
-		log.Panic(indexURL, "\n", err)
+		log.Panic(indexURL, "\n", err, "\n", string(body))
 	}
+
+	matchedBlacklisted := make(map[string]bool)
 
 	feed := &feeds.Rss{Feed: &feeds.Feed{
 		Title:       strings.Join(os.Args[1:], ", "),
@@ -87,13 +98,25 @@ func main() {
 		Description: strings.Join(os.Args[1:], ", ") + " - Gelbooru",
 	}}
 
+outer:
 	for _, p := range b.Post {
 		id := strconv.Itoa(p.ID)
 		createdAt, err := time.Parse(time.RubyDate, p.CreatedAt)
 		if err != nil {
 			log.Panic(err)
 		}
-		title := getTitleForImage(db, strings.Split(p.Tags, " "), id, os.Args[1:])
+
+		tags := strings.Split(p.Tags, " ")
+		if len(blacklist) > 0 {
+			for _, t := range tags {
+				if blacklist[t] {
+					matchedBlacklisted[t] = true
+					continue outer
+				}
+			}
+		}
+
+		title := getTitleForImage(db, tags, id, os.Args[1:])
 		if title == "" {
 			title = id
 		}
@@ -107,8 +130,22 @@ func main() {
 		})
 	}
 
+	blacklistSorted := []string{}
+	for t := range matchedBlacklisted {
+		blacklistSorted = append(blacklistSorted, t)
+	}
+	sort.Slice(blacklistSorted, func(i, j int) bool {
+		return blacklistSorted[i] < blacklistSorted[j]
+	})
+
+	blacklistString := strings.Join(blacklistSorted, "+-")
+
+	if len(blacklistString) != 0 {
+		feed.Link.Href += "+-" + blacklistString
+	}
+
 	rssFeed := feed.RssFeed()
-	rssFeed.Ttl = 60
+	rssFeed.Ttl = 120
 
 	feedXML, err := xml.Marshal(rssFeed.FeedXml())
 	if err != nil {
@@ -127,7 +164,7 @@ func openTagsDB() *leveldb.DB {
 	dir := path.Join(home, ".rss", "geltagdb")
 
 	db, err := leveldb.OpenFile(dir, nil)
-	if errors.IsCorrupted(err) {
+	if ldberrors.IsCorrupted(err) {
 		db, err = leveldb.RecoverFile(dir, nil)
 	}
 	if err != nil {
@@ -211,7 +248,7 @@ func loadMissingTags(db *leveldb.DB, tags []string) {
 		escaped = append(escaped, url.QueryEscape(html.UnescapeString(html.UnescapeString(t))))
 	}
 
-	url := tagsAPIPrefix + strings.Join(escaped, "+")
+	url := fmt.Sprintf(tagsAPIFormat, apiKey, strings.Join(escaped, "+"))
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -238,4 +275,55 @@ func loadMissingTags(db *leveldb.DB, tags []string) {
 			log.Panic(err)
 		}
 	}
+}
+
+func loadApiKey() string {
+	home := os.Getenv("HOME")
+	if home == "" {
+		return ""
+	}
+
+	fpath := path.Join(home, ".rss", "gelapi")
+
+	file, err := os.Open(fpath)
+	if errors.Is(err, os.ErrNotExist) {
+		return ""
+	}
+	if err != nil {
+		log.Panic("API key file exists but cannot be read", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func loadBlacklist() map[string]bool {
+	home := os.Getenv("HOME")
+	if home == "" {
+		return nil
+	}
+
+	fpath := path.Join(home, ".rss", "gelblacklist")
+
+	file, err := os.Open(fpath)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	if err != nil {
+		log.Panic("Tag blacklist exists but cannot be read", err)
+	}
+	defer file.Close()
+
+	out := make(map[string]bool)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" {
+			out[line] = true
+		}
+	}
+
+	return out
 }
