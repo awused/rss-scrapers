5de8c1dd95ce9ac4b1ee1bb561cf221da0d2da5f
Desuwa <desuwa@desuwa.net>
2025-12-31 14:39:35 -0800

    Update broken scraper

--- src/tfgames.rs
+++ src/tfgames.rs
@@ -2,14 +2,15 @@ use chrono::Utc;
 use color_eyre::Result;
 use color_eyre::eyre::OptionExt;
 use rss::{ChannelBuilder, GuidBuilder, ItemBuilder};
-use scraper::{CaseSensitivity, Html, Selector};
+use scraper::{Html, Selector};
 use tracing::error_span;
 
 pub fn get(game: String) -> Result<()> {
     let client = reqwest::blocking::Client::new();
 
     let url = format!("https://tfgames.site/?module=viewgame&id={game}");
-    let a_select = Selector::parse(".dltext > a").unwrap();
+    let a_select = Selector::parse(".download-link > a").unwrap();
+    let version_select = Selector::parse(".download-card-header").unwrap();
 
     let page = client.get(url.clone()).send()?.bytes()?;
     let _span = error_span!("response", page = %String::from_utf8_lossy(&page)).entered();
@@ -27,31 +28,25 @@ pub fn get(game: String) -> Result<()> {
 
     let now = Utc::now().to_rfc2822();
 
-    let mut version = String::new();
     let mut items = Vec::new();
 
-    for e in doc.select(&Selector::parse("div#downloads > *").unwrap()) {
-        if e.value().name() == "center" {
-            version = e.text().collect();
-            continue;
+    for e in doc.select(&Selector::parse("div#downloads-list > .download-card").unwrap()) {
+        let header = e.select(&version_select).next().ok_or_eyre("Missing version string")?;
+        let version: String = header.text().collect();
+        let version = version.trim();
+
+        for a in e.select(&a_select) {
+            let href = a.attr("href").ok_or_eyre("Download link missing url")?;
+
+            items.push(
+                ItemBuilder::default()
+                    .title(Some(format!("{version} {}", a.text().collect::<String>())))
+                    .link(Some(href.to_string()))
+                    .guid(Some(GuidBuilder::default().value(href.to_string() + version).build()))
+                    .pub_date(Some(now.clone()))
+                    .build(),
+            );
         }
-
-        if !e.value().has_class("dlcontainer", CaseSensitivity::CaseSensitive) {
-            continue;
-        }
-
-        let a = e.select(&a_select).next().ok_or_eyre("Missing download link")?;
-
-        let href = a.attr("href").ok_or_eyre("Download link missing url")?;
-
-        items.push(
-            ItemBuilder::default()
-                .title(Some(format!("{version} {}", a.text().collect::<String>())))
-                .link(Some(href.to_string()))
-                .guid(Some(GuidBuilder::default().value(href.to_string() + &version).build()))
-                .pub_date(Some(now.clone()))
-                .build(),
-        );
     }
 
     let feed = ChannelBuilder::default()
